3ee3e3e3b75490c3a1331e441f302fda65182616
William Li <wul@microsoft.com>
2018-03-05 12:28:41 -0800

    fix sh and test on unix

--- build.sh
+++ build.sh
@@ -1,3 +1,14 @@
+test=false
+while [[ $# > 0 ]]; do
+	lowerI="$(echo $1 | awk '{print tolower($0)}')"
+	case $lowerI in
+		--test)
+			test=true
+			shift 1
+			;;
+	esac
+done
+
 export DOTNET_SKIP_FIRST_TIME_EXPERIENCE=1
 export DOTNET_MULTILEVEL_LOOKUP=0
 DotNetCliVersion="2.1.300-preview1-008174"
@@ -5,20 +16,21 @@ DotNetRoot=".dotnet"
 DotNetInstallScript="$DotNetRoot/dotnet-install.sh"
 Dotnet="$DotNetRoot/dotnet"
 
-function CreateDirectory {
-  if [ ! -d "$1" ]
-  then
-    mkdir -p "$1"
-  fi
+function CreateDirectory() {
+	if [ ! -d "$1" ]; then
+		mkdir -p "$1"
+	fi
 }
 
-if [ ! -d "$DotNetInstallScript" ]; then
-  CreateDirectory "$DotNetRoot"
-  curl "https://dot.net/v1/dotnet-install.sh" -sSL -o "$DotNetInstallScript"
-	bash "$DotNetInstallScript" --version $DotNetCliVersion --install-dir $DotNetRoot
-fi
+CreateDirectory "$DotNetRoot"
+curl "https://dot.net/v1/dotnet-install.sh" -sSL -o "$DotNetInstallScript"
+bash "$DotNetInstallScript" --version $DotNetCliVersion --install-dir $DotNetRoot
 
 export PATH="$DotNetRoot:$PATH"
 
 dotnet build TRex.sln -r win10-x64
+if [ $test != "true" ]; then
+  dotnet test TRexLib.Tests/TRexLib.Tests.csproj
+fi
+
 dotnet pack t-rex/t-rex.csproj /p:packTool=true
