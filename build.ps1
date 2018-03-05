13c0f9dbb0decaff169dbaac332b218508e6f1d5
William Li <wul@microsoft.com>
2018-03-04 21:55:48 -0800

    WIP

--- build.ps1
+++ build.ps1
@@ -1,2 +1,19 @@
+$DotNetCliVersion = "2.1.300-preview1-008174"
+$DotNetRoot = ".dotnet"
+$DotNetInstallScript = Join-Path $DotNetRoot "dotnet-install.ps1"
+$Dotnet = Join-Path $DotNetRoot "dotnet"
+
+if (![System.IO.File]::Exists($Dotnet)) {
+    if (Test-Path $DotNetRoot) {
+        Remove-Item $DotNetRoot -recurse
+    }
+    New-Item -ItemType directory -Path $DotNetRoot
+    Invoke-WebRequest "https://dot.net/v1/dotnet-install.ps1" -UseBasicParsing -OutFile $DotNetInstallScript
+    Invoke-Expression -Command "$DotNetInstallScript -Version $DotNetCliVersion -InstallDir $DotNetRoot"
+}
+
+Write-Information "dotnet SDK exists skip"
+$env:PATH = "$DotNetRoot;$env:PATH"
+
 dotnet build TRex.sln -r win10-x64
 dotnet publish t-rex/t-rex.csproj -r win10-x64 -f netcoreapp2.0
